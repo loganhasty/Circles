e351e3ee4d24d2cb3c7324e578a0858d0c42abeb
Logan Hasty <loganhasty@gmail.com>
2018-01-25 18:11:37 -0500

    Add files via upload

---
+++ tutorial.js
@@ -0,0 +1,27 @@
+//Rectangle
+
+//c.fillStyle = "rgba(0, 255, 0, 0.5)";
+//c.fillRect(x, y, width, height);
+// c.fillRect(50, 50, 100, 100);
+
+
+//Line
+
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300,100);
+// c.lineTo(400,300);
+// c.moveTo(50, 300);
+// c.lineTo(400,300);
+// c.strokeStyle = "blue";
+// c.stroke();
+
+
+//Arc
+
+//c.arc(x, y, r, startAngle: Float, endAngle: Float drawCounterClockwise: Bool (false));
+// c.beginPath();
+// c.arc(300, 200, 30, 0, Math.PI * 2, false);
+// c.strokeStyle = "purple";
+// c.stroke();
+
